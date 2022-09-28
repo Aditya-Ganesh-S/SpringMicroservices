3ed56556bc54611f16f9bdc64b2f5a2370f75191
Aditya Ganesh Satish <satish.a@northeastern.edu>
2022-09-28 16:30:27 -0400

    Added i18n and rest api documentation swagger ui

--- rest-webservices/src/main/java/com/restWebServices/restwebservices/helloWorld/HelloWorld.java
+++ rest-webservices/src/main/java/com/restWebServices/restwebservices/helloWorld/HelloWorld.java
@@ -1,13 +1,28 @@
 package com.restWebServices.restwebservices.helloWorld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorld {
 
+    private MessageSource messageSource;
+
+    public HelloWorld(MessageSource messageSource){
+        this.messageSource = messageSource;
+    }
     @GetMapping("/hello-world")
     private String HelloWorld(){
         return "Hello World";
+    }
+
+    @GetMapping("/hello-world-i18n")
+    private String HelloWorldInternationalized(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, null, locale);
     }
 }
